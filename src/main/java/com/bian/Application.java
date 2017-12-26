21d26d5fb0e8402a44ebd9d2dcc55f2eb16c177c
cumtbzy2011 <597566475@qq.com>
2017-12-26 13:57:15 +0800

    Initial commit

---
+++ src/main/java/com/bian/Application.java
@@ -0,0 +1,15 @@
+package com.bian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bianzy
+ * @create 2017-12-22 14:03
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/bian/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.bian.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bianzy
+ * @create 2017-12-22 14:04
+ **/
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello() {
+        return "hello world!";
+    }
+}
